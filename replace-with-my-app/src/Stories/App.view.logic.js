23728e8f16c1c7c1e61f8cc2c92cdee7666880c0
Tom Parandyk <tom@uxtemple.com>
2020-01-25 18:14:58 +0100

    fixing flow

--- replace-with-my-app/src/Stories/App.view.logic.js
+++ replace-with-my-app/src/Stories/App.view.logic.js
@@ -1,8 +1,10 @@
 import App from './App.view.js';
 import React from 'react';
-
-function AppLogic() {
-  return <App />;
+import { Flow } from 'useFlow.js';
+export default function AppLogic() {
+  return (
+    <Flow>
+      <App />
+    </Flow>
+  );
 }
-
-export default AppLogic;
